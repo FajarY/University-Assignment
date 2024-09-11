6f2a082bf565ce3a50e2e99c81a2752a58a186b7
FajarY <fajaryasodana.dev@gmail.com>
2024-09-11 23:17:10 +0700

    PWEB 2

---
+++ Semester 3/Web Programming/Class 2/static/index.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('contact-button');
+
+button.onclick = () =>
+{
+    window.open('https://www.linkedin.com/in/fajar-yasodana-5b5208278/?originalSubdomain=id');
+}
